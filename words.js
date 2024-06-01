f3d53fc506dfa6ff14bbd256c3e177d2c85758f2
cs3-J-Porol-27 <143087024+cs3-J-Porol-27@users.noreply.github.com>
2024-06-01 18:12:31 +0800

    first commit

---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "Ecosystem",
+        hint: "A community of living organisms interacting with their physical environment."
+    },
+    {
+        word: "Herbivore",
+        hint: "An animal that primarily feed on plants."
+    },
+    {
+        word: "Rodents",
+        hint: "A group of mammals that includes mice, rats, and squirrels."
+    },
+    {
+        word: "Parasite",
+        hint: "An organism that lives on or in a host, causing harm to the host."
+    },
+    {
+        word: "Predation",
+        hint: "The preying of one animal on others."
+    },
+    {
+        word: "Migration",
+        hint: "Seasonal movement of animals from one region to another."
+    },
+    {
+        word: "Abiotic",
+        hint: "Non-living chemical and physical parts of the environment."
+    },
+    {
+        word: "Habitat",
+        hint: "The natural home or environment of an organism."
+    },
+    {
+        word: "Forest",
+        hint: "An ecosystem characterized by a high density of trees."
+    },
+    {
+        word: "Aquatic",
+        hint: "Ecosystems in water, including oceans, lakes, and rivers."
+    },
+    {
+        word: "Lichen",
+        hint: "A composite organism arising from algae or cyanobacteria living among filaments of fungi."
+    },
+    {
+        word: "PolarBear",
+        hint: "Apex predator of the Arctic ice."
+    },
+    {
+        word: "Clownfish",
+        hint: "Small, colorful fish that lives among sea anemones."
+    },
+    {
+        word: "Camel",
+        hint: "Large mammal known for its humps and ability to go without water for long periods."
+    },
+    {
+        word: "Tundra",
+        hint: "A cold, treeless ecosystem found in the Arctic and on mountaintops."
+    },
+    {
+        word: "Savannas",
+        hint: "A grassy plain in tropical and subtropical regions, with few trees."
+    },
+    {
+        word: "Owl",
+        hint: "Nocturnal bird known for its hooting."
+    },
+    {
+        word: "Deer",
+        hint: "Herbivore known for its antlers."
+    },
+    {
+        word: "Lion",
+        hint: "The king of the jungle, found in African grasslands."
+    },
+    {
+        word: "Meerkat",
+        hint: "A small mammal known for its social behavior and living in burrows."
+    },
+    {
+        word: "RattleSnake",
+        hint: "A venomous snake known for its rattling tail."
+    },
+    {
+        word: "Jaguar",
+        hint: "A large feline found in tropical rainforests."
+    },
+    {
+        word: "Squirrel",
+        hint: "A small rodent known for gathering nuts."
+    },
+]
